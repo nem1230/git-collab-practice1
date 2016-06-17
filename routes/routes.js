8749523fbfa2d2e5fa5c8b64135dc1c18c4b58bd
Hines Kao <hineskao@gmail.com>
2016-06-17 12:32:03 -0700

    completed routes

--- routes/routes.js
+++ routes/routes.js
@@ -1,2 +1,25 @@
 // TODO: Hines: Require express
 // TODO: Hines: Setup and Root Route
+
+var
+  express = require('express')
+
+app.get('/createQuestion', function(req, res){
+  console.log(req.query);
+
+  var title = "";
+  var content = "";
+  if (req.query.title){
+    title = req.query.title;
+  }
+  if (req.query.content){
+    content = req.query.content;
+  }
+  var q = new Question({title: title, content: content});
+  q.save(function(err, result){
+    if (err){console.log(err);}
+    else {
+      console.log(result);}
+      res.json(result);
+  });
+})
