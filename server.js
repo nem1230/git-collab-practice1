540267101411f900914a6277f6a19ea90c70218b
Ed Petil <e.petil.ms@gmail.com>
2016-06-17 12:33:25 -0700

    initial setu;

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 // TODO: ED: create the app.listen
 // TODO: ED: require express
 // TODO: ED: NPM INSTALL EXPRESS
+var
+
+  express = require('express')
+
+app.listen(3000, function(req, res) {
+  if (err) console.log(err)
+  res.json("Server is now running on 3000")
+})
